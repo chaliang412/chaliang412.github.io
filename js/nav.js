8b95dd0a2ce39b980445a93f744b978ce9687ac7
茶凉凉凉凉丶 <76806814+chaliang412@users.noreply.github.com>
2025-10-17 22:45:20 +0800

    Update nav.js

--- js/nav.js
+++ js/nav.js
@@ -1,1 +1,7 @@
+// 移动菜单切换
+      const mobileMenuBtn = document.getElementById("mobileMenuBtn");
+      const navMenu = document.getElementById("navMenu");
 
+      mobileMenuBtn.addEventListener("click", () => {
+        navMenu.classList.toggle("active");
+      });
